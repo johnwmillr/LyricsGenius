cb37117e8b2b7eb5b9c8ae26e7397e4f9b5ad908
johnwmillr <john.w.millr@gmail.com>
2017-07-09 16:20:55 -0500

    Clean up API access functions

--- getArtistLyrics.py
+++ getArtistLyrics.py
@@ -56,42 +56,9 @@ def get_song_and_artist_ids(song_title, artist_name):
     
     return song_api, artist_api
 
-def get_artist_song_ids(artist_name):
-    obj = search_genius(artist_name)
-    song_title = obj['response']['hits'][0]['result']['title']
-    song_id, artist_id = get_song_and_artist_ids(song_title,artist_name)
-    id_num = int(artist_id.split('/')[-1])
-
-    # Okay, we have the artist API id, let's get a list of all of their songs on Genius
-    all_song_ids = []
-    page = 1
-    print('\nGetting song IDs from Genius.com...')
-    while True:
-        request = urllib2.Request(genius_url + artist_id + '/songs' + '?page=%d' % page)
-        request.add_header("Authorization", "Bearer " + client_access_token)
-        request.add_header("User-Agent", "curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)")
-        response = urllib2.urlopen(request, timeout=4)
-        raw = response.read()
-        json_obj = json.loads(raw)      
-        songs = json_obj['response']['songs']
-        
-        # Keep track of song API paths if the primary artist is correct
-        [all_song_ids.append(song['api_path']) for song in songs if song['primary_artist']['id'] == id_num]            
-
-        num_songs = len(songs)
-        print("Found {0} possible songs on page {1}.".format(num_songs, page))    
-        if json_obj['response']['next_page']==None:
-            if page==1 & num_songs == 0:
-                print("No results for: " + search_term)
-            break              
-        page += 1
-        
-    print('Total songs found: {0}.'.format(len(all_song_ids)))
-    return all_song_ids
-
-def lyrics_from_song_api_path(song_api_path):
-    # Use BeautifulSoup to scrape lyrics off of a Genius song URL
-    querystring = genius_url + song_api_path      
+def get_genius_api_item(api_path):
+    base_url = "http://api.genius.com"
+    querystring = base_url + api_path      
     request = urllib2.Request(querystring)
     request.add_header("Authorization", "Bearer " + client_access_token)   
     request.add_header("User-Agent", "curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)") #Must include user agent of some sort, otherwise 403 returned
@@ -104,13 +71,48 @@ def lyrics_from_song_api_path(song_api_path):
             continue
         break
 
-    json_obj = json.loads(raw)  
+    return json.loads(raw)
+
+def get_artist_song_ids(artist_name):
+    obj = search_genius(artist_name)
+    song_title = obj['response']['hits'][0]['result']['title']
+    song_id, artist_id = get_song_and_artist_ids(song_title,artist_name)
+    id_num = int(artist_id.split('/')[-1])
+
+    # Okay, we have the artist API id, let's get a list of all of their songs on Genius
+    all_song_ids = []
+    page = 1
+    print('\nGetting song IDs from Genius.com...')
+    while True:
+        # Access the Genius API to get artist's songs
+        artist_api_path = artist_id + '/songs' + '?page=%d' % page
+        json_obj = get_genius_api_item(artist_api_path)     
+        songs = json_obj['response']['songs']
+        
+        # Only keep a song's API path if its primary artist is correct
+        [all_song_ids.append(song['api_path']) for song in songs if song['primary_artist']['id'] == id_num]            
+
+        # Break the loop if we're on the last page
+        num_songs = len(songs)
+        print("Found {0} possible songs on page {1}.".format(num_songs, page))    
+        if json_obj['response']['next_page']==None:
+            if page==1 & num_songs == 0:
+                print("No results for: " + search_term)
+            break              
+        page += 1
+        
+    print('Total songs found: {0}.'.format(len(all_song_ids)))
+    return all_song_ids
+
+def scrape_lyrics_from_song_api(song_api_path):
+    # Use BeautifulSoup to scrape lyrics off of a Genius song URL
+    json_obj = get_genius_api_item(song_api_path)
 
     # Get the URL to the song lyrics
     path = json_obj['response']['song']['path']
     page_url = "http://genius.com" + path
     page = requests.get(page_url)
-    html = BeautifulSoup(page.text, "html.parser")    
+    html = BeautifulSoup(page.text, "html.parser")
     [h.extract() for h in html('script')]        
     lyrics = html.find("div", class_="lyrics").get_text().encode('ascii','ignore').decode('ascii')
     lyrics = re.sub('\[.*\]','',lyrics) # Remove [Verse] and [Bridge] stuff
@@ -137,7 +139,7 @@ def main():
     print('\nWriting lyrics to file...')
     for i in range(len(all_song_ids)):
         print('Writing song {0} of {1}.'.format(i+1,len(all_song_ids)))
-        lyrics = lyrics_from_song_api_path(all_song_ids[i])
+        lyrics = scrape_lyrics_from_song_api(all_song_ids[i])
         write_lyrics_to_file(lyrics,artist_name)
 
     print('Done. Total time: %0.1f minutes.' % ((time.time()-t)/60.0))
