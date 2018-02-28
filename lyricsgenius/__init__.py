0179ff0f6de195f8957b71c8109a2b680d13ef3c
johnwmillr <john.w.millr@gmail.com>
2018-02-28 15:12:31 -0600

    Add check for Python 3 upon import

--- lyricsgenius/__init__.py
+++ lyricsgenius/__init__.py
@@ -9,4 +9,6 @@ __url__ = 'https://github.com/johnwmillr/GeniusAPI'
 __description__ = 'A Python wrapper around the Genius API'
 __license__ = 'MIT'
 
+import sys
+assert sys.version_info[0] == 3, "LyricsGenius requires Python 3."
 from .api import Genius
