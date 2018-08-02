f634d5267ecec8233d859ca9e89d7dbf52198a79
johnwmillr <john.w.millr@gmail.com>
2018-08-02 13:56:32 -0500

    Replace _API class with new session-based API class

--- lyricsgenius/__init__.py
+++ lyricsgenius/__init__.py
@@ -8,9 +8,8 @@ __author__ = 'John W. Miller'
 __url__ = 'https://github.com/johnwmillr/GeniusAPI'
 __description__ = 'A Python wrapper around the Genius API'
 __license__ = 'MIT'
-__version__ = '0.7.0'
+__version__ = '0.9.0'
 
 import sys
 assert sys.version_info[0] == 3, "LyricsGenius requires Python 3."
 from .api import Genius
-from .api import API
