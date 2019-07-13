1f6818687ec4250ea2b0992965f77e988ede06fc
ludehon <luciendehon@gmail.com>
2019-07-13 15:13:28 +0200

    removed test file

--- lyricsgenius/test.py
+++
@@ -1,8 +0,0 @@
-from api import Genius
-genius = Genius("8JuZA2LLElhUEwq_NW25t46s5oLYqjNhTXRoUxKmRENwH8LnWzorrhKl-thRS7E3")
-
-anno = genius.get_song_annotations(873)
-print(len(anno))
-for a in anno:
-    print("\n------------")
-    print(a)
