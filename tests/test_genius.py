ae7af150253edea94989682215635f2b4b75fe57
John Miller <john.miller@billups.com>
2020-04-22 08:47:10 -0700

    Increase sleep time on tests to 1.0 s

--- tests/test_genius.py
+++ tests/test_genius.py
@@ -9,7 +9,7 @@ from lyricsgenius.utils import sanitize_filename
 # Import client access token from environment variable
 client_access_token = os.environ.get("GENIUS_CLIENT_ACCESS_TOKEN", None)
 assert client_access_token is not None, "Must declare environment variable: GENIUS_CLIENT_ACCESS_TOKEN"
-genius = Genius(client_access_token, sleep_time=0.5, timeout=15)
+genius = Genius(client_access_token, sleep_time=1.0, timeout=15)
 
 
 class TestEndpoints(unittest.TestCase):
