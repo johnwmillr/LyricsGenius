7534fb628a74eaadd340c0b740aa513aac2f5eeb
Allerter <45076212+Allerter@users.noreply.github.com>
2020-09-27 10:08:23 +0330

    Revert "updated to fit origin"
    This reverts commit 098ae1997ceb589248b64b7e63bb77e1b54d917a.

--- lyricsgenius/api/base.py
+++ lyricsgenius/api/base.py
@@ -1,5 +1,5 @@
 import requests
-from requests.exceptions import HTTPError, Timeout
+from requests.exceptions import Timeout
 import time
 
 
@@ -45,22 +45,12 @@ class Sender(object):
             response = self._session.request(method, uri,
                                              timeout=self.timeout,
                                              params=params_)
-            response.raise_for_status()
         except Timeout as e:
-            error = "Request timed out:\n{e}".format(e=e)
-            raise Timeout(error)
-        except HTTPError as e:
-            error = str(e)
-            res = e.response.json()
-            description = (res['meta']['message']
-                           if res.get('meta')
-                           else res.get('error_description'))
-            error += '\n{}'.format(description) if description else ''
-            raise HTTPError(response.status_code, error)
+            print("Timeout raised and caught:\n{e}".format(e=e))
 
         if header:
             self._session.headers['authorization'] = header
 
         # Enforce rate limiting
         time.sleep(max(self._SLEEP_MIN, self.sleep_time))
-        return response.json()['response']
+        return response.json()['response'] if response else None
