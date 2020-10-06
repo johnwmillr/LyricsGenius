b9ff3169cf2806ba7c9f4225ef7bb31940477de3
Allerter <45076212+Allerter@users.noreply.github.com>
2020-10-06 20:06:13 +0330

    handle headers better in _make_request

--- lyricsgenius/api/base.py
+++ lyricsgenius/api/base.py
@@ -26,9 +26,7 @@ class Sender(object):
             'application': 'LyricsGenius',
             'User-Agent': 'https://github.com/johnwmillr/LyricsGenius'
         }
-        self.access_token = access_token
-        if access_token is not None:
-            self._session.headers['authorization'] = 'Bearer ' + self.access_token
+        self.access_token = 'Bearer ' + access_token
         self.response_format = response_format.lower()
         self.timeout = timeout
         self.sleep_time = sleep_time
@@ -44,10 +42,10 @@ class Sender(object):
         """Makes a request to the API."""
         if public_api:
             uri = self.PUBLIC_API_ROOT
-            header = self._session.headers.pop('authorization', None)
+            header = None
         else:
             uri = self.API_ROOT
-            header = None
+            header = {'Authorization': self.access_token}
         uri += path
 
         params_ = params_ if params_ else {}
@@ -58,6 +56,7 @@ class Sender(object):
             response = self._session.request(method, uri,
                                              timeout=self.timeout,
                                              params=params_,
+                                             headers=header,
                                              **kwargs)
             response.raise_for_status()
         except Timeout as e:
@@ -71,9 +70,6 @@ class Sender(object):
                            else res.get('error_description'))
             error += '\n{}'.format(description) if description else ''
             raise HTTPError(response.status_code, error)
-
-        if header:
-            self._session.headers['authorization'] = header
 
         # Enforce rate limiting
         time.sleep(max(self._SLEEP_MIN, self.sleep_time))
