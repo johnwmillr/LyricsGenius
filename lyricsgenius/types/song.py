bb948b1fe83dd86d27b68b2cd946c10a05c6de89
Stefano Pigozzi <me@steffo.eu>
2021-02-10 15:27:08 +0100

    🔧 Add fields in song for the new fields

--- lyricsgenius/types/song.py
+++ lyricsgenius/types/song.py
@@ -23,18 +23,42 @@ class Song(BaseEntity):
 
         self.annotation_count = body['annotation_count']
         self.api_path = body['api_path']
+        self.apple_music_id = body.get('apple_music_id')
+        self.apple_music_player_url = body.get('apple_music_player_url')
+        self.description = body.get('description')
+        self.embed_content = body.get('embed_content')
+        self.featured_video = body.get('featured_video')
         self.full_title = body['full_title']
         self.header_image_thumbnail_url = body['header_image_thumbnail_url']
         self.header_image_url = body['header_image_url']
         self.lyrics_owner_id = body['lyrics_owner_id']
+        self.lyrics_placeholder_reason = body.get('lyrics_placeholder_reason')
         self.lyrics_state = body['lyrics_state']
         self.path = body['path']
         self.pyongs_count = body['pyongs_count']
+        self.recording_location = body.get('recording_location')
+        self.release_date = body.get('release_date')
+        self.release_date_for_display = body.get('release_date_for_display')
         self.song_art_image_thumbnail_url = body['song_art_image_thumbnail_url']
         self.song_art_image_url = body['song_art_image_url']
         self.title = body['title']
         self.title_with_featured = body['title_with_featured']
         self.url = body['url']
+
+        # TODO: Create types for the following keys
+        self.custom_performances = body.get("custom_performances")
+        self.description_annotation = body.get("description_annotation")
+        self.featured_artists = body.get("featured_artists")
+        self.lyrics_marked_complete_by = body.get("lyrics_marked_complete_by")
+        self.media = body.get("media")
+        producer_artists = body.get("producer_artists")
+        if isinstance(producer_artists, list):
+            producer_artists = list(map(lambda a: Artist(client, a), producer_artists))
+        self.producer_artists = producer_artists
+        self.song_relationships = body.get("song_relationships")
+        self.verified_annotations_by = body.get("verified_annotations_by")
+        self.verified_contributors = body.get("verified_contributors")
+        self.verified_lyrics_by = body.get("verified_lyrics_by")
 
     def to_dict(self):
         body = super().to_dict()
