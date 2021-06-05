1ecf987d477f47a99b84e7afbfe7434b89a8e05f
allerter <8884518-allerter@users.noreply.gitlab.com>
2021-06-05 13:19:58 +0430

    Define uses_public_api decorator

--- lyricsgenius/errors.py
+++ lyricsgenius/errors.py
@@ -1,2 +1,6 @@
 class InvalidStateError(Exception):
     """Exception for non-matching states."""
+
+
+class PublicAPIForbiddenError(Exception):
+    """Exception for forbidden access to PublicAPI methods."""
