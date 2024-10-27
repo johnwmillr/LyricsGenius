96fa8d3349e2e41b192cd3ce78a66050d5f8f4f7
Kurt <38019652+iiKurt@users.noreply.github.com>
2024-10-28 05:10:46 +1300

    fix lyrics search (#236)

--- lyricsgenius/genius.py
+++ lyricsgenius/genius.py
@@ -208,10 +208,10 @@ class Genius(API, PublicAPI):
         sections = sorted(response['sections'],
                           key=lambda sect: sect['type'] == type_)
 
-        hits = [hit for hit in top_hits if hit['type'] == type_]
+        hits = [hit for hit in top_hits if hit['index'] == type_]
         hits.extend([hit for section in sections
                      for hit in section['hits']
-                     if hit['type'] == type_])
+                     if hit['index'] == type_])
 
         for hit in hits:
             item = hit['result']
