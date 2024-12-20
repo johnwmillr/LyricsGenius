1e65e4dd2c2afdb3461c8d356b41bcc703ca0efa
Vauth <122077080+Vauth@users.noreply.github.com>
2024-12-20 14:30:21 +0330

    UPDATE add[proxy]

--- lyricsgenius/api/api.py
+++ lyricsgenius/api/api.py
@@ -51,6 +51,7 @@ class API(Sender):
 
     def __init__(self,
                  access_token,
+                 proxy=None,
                  response_format='plain',
                  timeout=5,
                  sleep_time=0.2,
@@ -58,6 +59,7 @@ class API(Sender):
                  ):
         super().__init__(
             access_token=access_token,
+            proxy=proxy,
             response_format=response_format,
             timeout=timeout,
             sleep_time=sleep_time,
