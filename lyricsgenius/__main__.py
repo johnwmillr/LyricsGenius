57d68c1dda26458ede59cfc26a446ea5cf424b19
John W. Miller <johnwmillr@users.noreply.github.com>
2025-04-11 22:43:35 -0700

    Improve cli tool (#302)
    * Overhaul the CLI
    
      - Removes '--stdout' and adds '--format'
      - Prints to stdout by default now
      - Verbosity is off by default now
      - Adds new optional CLI arguments (token, overwrite)
      - Changes default format to 'txt' instead of 'json'
    
    * Replace the repeated search code with a callable object

--- lyricsgenius/__main__.py
+++ lyricsgenius/__main__.py
@@ -10,99 +10,106 @@ from . import Genius
 from .utils import safe_unicode
 
 
+class Searcher:
+    """Executes the search specified by the CLI args"""
+
+    def __init__(self, api, search_type):
+        self.api = api
+        self.search_type = search_type
+        if search_type == "song":
+            self.search_func = api.search_song
+        elif search_type == "artist":
+            self.search_func = api.search_artist
+        elif search_type == "album":
+            self.search_func = api.search_album
+        else:
+            raise ValueError(f"Unknown search type: {search_type}")
+
+    def __call__(self, args):
+        result = self.search_func(*args.terms)
+        if not result:
+            return
+        for format in args.format:
+            if not args.save:
+                print(result.to_text() if format == "txt" else result.to_json())
+            else:
+                if args.verbose:
+                    print(
+                        f"Saving lyrics to '{safe_unicode(result.title)}' in {format.upper()} format..."
+                    )
+                result.save_lyrics(
+                    extension=format, overwrite=True if args.overwrite else False
+                )
+
+
 def main(args=None):
     msg = "Download song lyrics from Genius.com"
-    parser = argparse.ArgumentParser(description=msg)
-    parser.add_argument(
+    parser = argparse.ArgumentParser(prog="lyricsgenius", description=msg)
+    positional = parser.add_argument_group("Required Arguments")
+    positional.add_argument(
         "search_type",
         type=str.lower,
         choices=["song", "artist", "album"],
         help="Specify whether search is for 'song', 'artist' or 'album'.",
     )
-    parser.add_argument("terms", type=str, nargs="+", help="Provide terms for search")
-    parser.add_argument(
+    positional.add_argument(
+        "terms",
+        type=str,
+        nargs="+",
+        help="Provide terms for the search (e.g. 'All You Need Is Love' 'The Beatles').",
+    )
+
+    optional = parser.add_argument_group("Optional Arguments")
+    optional.add_argument(
+        "-f",
+        "--format",
+        type=str.lower,
+        nargs="+",
+        default=["txt"],
+        choices=["txt", "json"],
+        help="Specify output format(s): 'txt' (default) or 'json'. You can specify multiple formats.",
+    )
+    optional.add_argument(
+        "-s",
         "--save",
-        type=str,
-        nargs="*",
-        default="json",
-        const=None,
-        choices=["json", "txt"],
-        help="Specify one or more formats to save output: 'json' (default) or 'txt'",
+        action="store_true",
+        help="Save the lyrics to a file in the specified format instead of printing to stdout",
     )
-    parser.add_argument(
-        "--stdout",
-        type=str,
-        nargs="?",
-        const="json",
-        choices=["json", "txt"],
-        help="Print output to stdout in 'json' or 'txt' format",
+    optional.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite the file if it already exists",
     )
-    parser.add_argument(
+    optional.add_argument(
+        "-n",
         "--max-songs",
         type=int,
         help="Specify number of songs when searching for artist",
     )
-    parser.add_argument(
-        "-q", "--quiet", action="store_true", help="Turn off the API verbosity"
+    optional.add_argument(
+        "-t",
+        "--token",
+        type=str,
+        default=None,
+        help="Specify your Genius API access token (optional). If not provided, it will be read from the GENIUS_ACCESS_TOKEN environment variable.",
+    )
+    optional.add_argument(
+        "-v", "--verbose", action="store_true", help="Turn on the API verbosity"
     )
     args = parser.parse_args()
 
     # Create an instance of the Genius class
-    access_token = os.environ.get("GENIUS_ACCESS_TOKEN", None)
-    msg = "Must declare environment variable: GENIUS_ACCESS_TOKEN"
-    assert access_token, msg
-    api = Genius(access_token, timeout=10)
-    if args.quiet:
-        api.verbose = False
+    token = args.token if args.token else os.environ.get("GENIUS_ACCESS_TOKEN", None)
+    if token is None:
+        raise ValueError(
+            "Must provide access token either as an argument or as an environment variable."
+        )
+
+    api = Genius(token, verbose=args.verbose, timeout=10)
 
     # Handle the command-line inputs
-    if args.search_type == "song":
-        song = api.search_song(*args.terms)
-        if not song:
-            if not args.quiet:
-                print("Could not find specified song. Check spelling?")
-            return
-        if args.stdout:
-            print(song.to_text() if args.stdout == "txt" else song.to_json())
-        if args.save:
-            for format in args.save:
-                if not args.quiet:
-                    print(
-                        f"Saving lyrics to '{safe_unicode(song.title)}' in {format.upper()} format..."
-                    )
-                song.save_lyrics(extension=format)
-    elif args.search_type == "artist":
-        artist = api.search_artist(
-            args.terms[0], max_songs=args.max_songs, sort="popularity"
-        )
-        if not artist:
-            if not args.quiet:
-                print("Could not find specified artist. Check spelling?")
-            return
-        if args.stdout:
-            print(artist.to_json() if args.stdout == "json" else artist.to_text())
-        if args.save:
-            for format in args.save:
-                if not args.quiet:
-                    print(
-                        f"Saving '{safe_unicode(artist.name)}' lyrics in {format.upper()} format..."
-                    )
-                artist.save_lyrics(extension=format)
-    elif args.search_type == "album":
-        album = api.search_album(*args.terms)
-        if not album:
-            if not args.quiet:
-                print("Could not find specified album. Check spelling?")
-            return
-        if args.stdout:
-            print(album.to_json() if args.stdout == "json" else album.to_text())
-        if args.save:
-            for format in args.save:
-                if not args.quiet:
-                    print(
-                        f"Saving '{safe_unicode(album.name)}' lyrics in {format.upper()} format..."
-                    )
-                album.save_lyrics(extension=format)
+    Searcher(api, args.search_type)(args)
 
 
 if __name__ == "__main__":
