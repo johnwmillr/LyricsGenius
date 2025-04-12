63f442f2844a548ca3bf7fdee9c1f243a8baa88f
John William Ruth Miller <john.w.millr@gmail.com>
2025-04-11 22:03:56 -0700

    Overhaul the CLI
      - Removes '--stdout' and adds '--format'
      - Prints to stdout by default now
      - Verbosity is off by default now
      - Adds new optional CLI arguments (token, overwrite)
      - Changes default format to 'txt' instead of 'json'

--- lyricsgenius/__main__.py
+++ lyricsgenius/__main__.py
@@ -12,48 +12,69 @@ from .utils import safe_unicode
 
 def main(args=None):
     msg = "Download song lyrics from Genius.com"
-    parser = argparse.ArgumentParser(description=msg)
-    parser.add_argument(
+    parser = argparse.ArgumentParser(prog="lyricsgenius", description=msg)
+    positional = parser.add_argument_group("Required Arguments")
+    positional.add_argument(
         "search_type",
         type=str.lower,
         choices=["song", "artist", "album"],
         help="Specify whether search is for 'song', 'artist' or 'album'.",
     )
-    parser.add_argument("terms", type=str, nargs="+", help="Provide terms for search")
-    parser.add_argument(
+    positional.add_argument(
+        "terms",
+        type=str,
+        nargs="+",
+        help="Provide terms for the search (e.g. 'All You Need Is Love' 'The Beatles').",
+    )
+
+    optional = parser.add_argument_group("Optional Arguments")
+    optional.add_argument(
+        "-f",
+        "--format",
+        type=str.lower,
+        nargs="+",
+        default=["txt"],
+        choices=["txt", "json"],
+        help="Specify output format(s): 'txt' (default) or 'json'. You can specify multiple formats.",
+    )
+    optional.add_argument(
+        "-s",
         "--save",
-        type=str,
-        nargs="*",
-        default="json",
-        const=None,
-        choices=["json", "txt"],
-        help="Specify one or more formats to save output: 'json' (default) or 'txt'",
+        action="store_true",
+        help="Save the lyrics to a file in the specified format instead of printing to stdout",
     )
-    parser.add_argument(
-        "--stdout",
-        type=str,
-        nargs="?",
-        const="json",
-        choices=["json", "txt"],
-        help="Print output to stdout in 'json' or 'txt' format",
+    optional.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite the file if it already exists",
     )
-    parser.add_argument(
+    optional.add_argument(
+        "-n",
         "--max-songs",
         type=int,
         help="Specify number of songs when searching for artist",
     )
-    parser.add_argument(
-        "-q", "--quiet", action="store_true", help="Turn off the API verbosity"
+    optional.add_argument(
+        "-t",
+        "--token",
+        type=str,
+        default=None,
+        help="Specify your Genius API access token (optional). If not provided, it will be read from the GENIUS_ACCESS_TOKEN environment variable.",
+    )
+    optional.add_argument(
+        "-v", "--verbose", action="store_true", help="Turn on the API verbosity"
     )
     args = parser.parse_args()
 
     # Create an instance of the Genius class
-    access_token = os.environ.get("GENIUS_ACCESS_TOKEN", None)
-    msg = "Must declare environment variable: GENIUS_ACCESS_TOKEN"
-    assert access_token, msg
-    api = Genius(access_token, timeout=10)
-    if args.quiet:
-        api.verbose = False
+    token = args.token if args.token else os.environ.get("GENIUS_ACCESS_TOKEN", None)
+    if token is None:
+        raise ValueError(
+            "Must provide access token either as an argument or as an environment variable."
+        )
+
+    api = Genius(token, verbose=args.verbose, timeout=10)
 
     # Handle the command-line inputs
     if args.search_type == "song":
@@ -62,15 +83,17 @@ def main(args=None):
             if not args.quiet:
                 print("Could not find specified song. Check spelling?")
             return
-        if args.stdout:
-            print(song.to_text() if args.stdout == "txt" else song.to_json())
-        if args.save:
-            for format in args.save:
+        for format in args.format:
+            if not args.save:
+                print(song.to_text() if format == "txt" else song.to_json())
+            else:
                 if not args.quiet:
                     print(
                         f"Saving lyrics to '{safe_unicode(song.title)}' in {format.upper()} format..."
                     )
-                song.save_lyrics(extension=format)
+                song.save_lyrics(
+                    extension=format, overwrite=True if args.overwrite else False
+                )
     elif args.search_type == "artist":
         artist = api.search_artist(
             args.terms[0], max_songs=args.max_songs, sort="popularity"
@@ -79,15 +102,17 @@ def main(args=None):
             if not args.quiet:
                 print("Could not find specified artist. Check spelling?")
             return
-        if args.stdout:
-            print(artist.to_json() if args.stdout == "json" else artist.to_text())
-        if args.save:
-            for format in args.save:
+        for format in args.format:
+            if not args.save:
+                print(artist.to_json() if format == "json" else artist.to_text())
+            else:
                 if not args.quiet:
                     print(
                         f"Saving '{safe_unicode(artist.name)}' lyrics in {format.upper()} format..."
                     )
-                artist.save_lyrics(extension=format)
+                artist.save_lyrics(
+                    extension=format, overwrite=True if args.overwrite else False
+                )
     elif args.search_type == "album":
         album = api.search_album(*args.terms)
         if not album:
@@ -102,7 +127,9 @@ def main(args=None):
                     print(
                         f"Saving '{safe_unicode(album.name)}' lyrics in {format.upper()} format..."
                     )
-                album.save_lyrics(extension=format)
+                album.save_lyrics(
+                    extension=format, overwrite=True if args.overwrite else False
+                )
 
 
 if __name__ == "__main__":
